139ba82723fb6e1fa633f1da726333ceaab28559
José Victor Pereira Lima <josevictorp81@gmail.com>
2024-02-27 16:24:39 -0300

    save notes on app state and pass a function to child element as prop

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import logo from './assets/Logo.svg'
 import { NewNoteCard } from './components/new-note-card'
-
 import { NoteCard } from './components/note-card'
 
+type Note = {
+  id: string,
+  date: Date,
+  content: string
+}
+
 export function App() {
-   return (
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
+
+  return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="NLW Expert" />
       <form className='w-full'>
@@ -17,10 +36,9 @@ export function App() {
       <div className='h-px bg-slate-700' />
 
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        <NoteCard note={{date: new Date(), content:'Olá'}} />
-
+        {notes.map(note => <NoteCard key={note.id} note={note} />)}
       </div>
     </div>
   )
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
-export function NewNoteCard(){
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard({onNoteCreated}: NewNoteCardProps){
 
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
   const [content, setContent] = useState('')
@@ -21,9 +25,10 @@ export function NewNoteCard(){
 
   function handleSaveNote(event: FormEvent){
     event.preventDefault()
-    console.log(content);
-    toast.success('Nota criada com sucesso!')
+    onNoteCreated(content)
+    setContent('')
     setShouldShowOnboarding(true)
+    toast.success('Nota criada com sucesso!')
   }
 
   return (
@@ -58,6 +63,7 @@ export function NewNoteCard(){
               <textarea autoFocus 
               className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none' 
               onChange={handleContentChenged}
+              value={content}
               />
             )}
           </div>
